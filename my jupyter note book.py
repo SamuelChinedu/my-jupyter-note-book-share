f165815a8e24235002e750d444ab2e174a1022d0
SamuelChinedu <85741644+SamuelChinedu@users.noreply.github.com>
2022-07-06 03:47:53 -0700

    Add files via upload

---
+++ my jupyter note book.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>My jupyter Notebook on IBM watson studio</h1>
+
+# <h2>Name: Nwabueze Samuel Chinedu </h2>
+# 
+
+# <p> I am a student </p>
+
+# <i>I am interseted in data science because i hope to use it to make make impact in the world <i>
+
+# <h3>my code in cell5 five is going to show to to calcute 1+33</h3>
+
+# In[2]:
+
+
+print(1+33)
+
+
+# In[ ]:
+
+
+
+
